98fdf4c9320729c8febf2b869cdd5c24c790b20b
Walber Melo <dev4wm@gmail.com>
2022-04-08 14:58:25 +0100

    add section profile validation

---
+++ src/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Select Elements
+let form = document.getElementById("profile-form");
+let errorMsg = document.querySelectorAll(".error");
+console.log(errorMsg);
+let btnNext = document.querySelector(".btn-next");
+
+//  Function profile
+let requirement = function () {
+  for (const profile of form) {
+    if (profile.value.trim() === "") {
+      profile.style.border = "2px solid red";
+      profile.placeholder = "Field required";
+    }
+  }
+};
+
+//Button next
+btnNext.addEventListener("click", (e) => {
+  e.preventDefault();
+  requirement();
+});
