4c2d297b8ba2e37cbd426100fee763f9136092c7
jose-cuevas <jgutifer@gmail.com>
2022-04-08 13:34:25 +0200

    starting js

---
+++ src/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Select DOM
+const formData = document.getElementsByTagName("form");
+const userName = document.getElementById("name");
+console.log(userName);
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("password");
+
+const firstName = document.getElementById("fname").value;
+console.log(firstName);
+const lastName = document.getElementById("lname");
+const birthDate = document.getElementById("birthday");
+const address_1 = document.getElementById("address1");
+const address_2 = document.getElementById("address2");
+
+// Engine function for requirement
+// let requirement = (id, message) => {
+//   if (id.value.trim() === " ") {
+
+//   }
+// };
+
+//target "form" and add submit event listener
+// formData.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   requirement(userName, "Username cannot be blank");
+
+// }
+
+// ------------------------------------
+// userName maxlength and min length
+const maxLength = (str) => {
+  if (isNaN(str) && str.length > 5 && str.length < 20) {
+    alert("error");
+  }
+};
+
+firstName.addEventListener("click", maxLength);
