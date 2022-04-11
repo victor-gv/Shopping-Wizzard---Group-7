9dd58cd71b4a9f6be1e9188d858a9c6f5e825bff
jose-cuevas <jgutifer@gmail.com>
2022-04-11 17:08:13 +0200

    begginig adresss events

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// * DOM ELEMENTS
 // Profile Elements
 const form = document.getElementById("profile-form");
 
@@ -17,6 +18,31 @@ const errorPassword = document.getElementById("error-password");
 const confirmPassword = document.getElementById("confirm-password");
 const errorConfirmPassword = document.getElementById("error-confirmPassword");
 
+// Adress Elements
+const firstName = document.getElementById("fname");
+const errorfirstName = document.getElementById("fname-error");
+
+const lastName = document.getElementById("lname");
+const errorLastName = document.getElementById("lname-error");
+
+const birthday = document.getElementById("birthday");
+const errorBirthday = document.getElementById("birthday-error");
+
+const address_1 = document.getElementById("address1");
+const errorAddress_1 = document.getElementById("address1-error");
+
+const address_2 = document.getElementById("address2");
+const errorAddress_2 = document.getElementById("address2-error");
+
+const postalCode = document.getElementById("postal-code");
+const errorpostalCode = document.getElementById("postal-code-error");
+
+const phone = document.getElementById("phone");
+// const errorpostalCode = document.getElementById("postal-code-error");
+
+// ----------------------------
+// * EVENTS
+// ----------------------------
 // Profile Events
 userName.addEventListener("focus", changeStyle);
 userName.addEventListener("blur", validateProfile);
@@ -27,14 +53,30 @@ userEmail.addEventListener("blur", validateProfile);
 userPassword.addEventListener("focus", changeStyle);
 userPassword.addEventListener("blur", validateProfile);
 
-confirmPassword.addEventListener("focus", changeStyle);
-confirmPassword.addEventListener("blur", validateProfile);
+postalCode.addEventListener("focus", changeStyle);
+postalCode.addEventListener("blur", validateProfile);
 
+// ----------------------------
+// Address Events
+firstName.addEventListener("focus", changeStyle);
+firstName.addEventListener("blur", validateAddress);
+
+lastName.addEventListener("focus", changeStyle);
+lastName.addEventListener("blur", validateAddress);
+
+lastName.addEventListener("focus", changeStyle);
+errorLastName.addEventListener("blur", validateAddress);
+
+phone.addEventListener("focus", changeStyle);
+phone.addEventListener("blur", validateAddress);
+
+// PENDING, it´s not mandatory
 function changeStyle() {}
 
+// Storing user data
 const userData = {};
-// const errorMessage = {};
 
+// validating data from Profile Page
 function validateProfile() {
   // UserName Validation
   if (userName.value.trim() === "" || userName.value === null) {
@@ -71,6 +113,21 @@ function validateProfile() {
     errorConfirmPassword.value = "Error! different passwords";
   } else {
     userData.password = confirmPassword.value;
+  }
+}
+
+function validateAddress() {
+  console.log("blur works");
+  // First name validation
+  if (firstName.value.trim() === "" || firstName.value === null) {
+    errorfirstName.textContent = "Name is required";
+  } else if (firstName.value.length < 5 || firstName.value.length > 20) {
+    errorfirstName.textContent = "Name length between 5 and 20 characters";
+  } else if (firstName.value.includes(" ")) {
+    errorfirstName.textContent = "Name can´t have space";
+  } else {
+    errorfirstName.style.display = "none";
+    userData.username = firstName.value;
   }
 }
 
