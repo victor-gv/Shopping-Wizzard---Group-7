db423e9bebc2febbf3466357628ede3ef6bcc4d3
jose-cuevas <jgutifer@gmail.com>
2022-04-11 11:56:06 +0200

    beginnig js

--- src/script.js
+++ src/script.js
@@ -1,21 +1,41 @@
 "use strict";
 
 // Select Elements
-let form = document.getElementById("profile-form");
-let btnNext = document.querySelector(".btn-next");
+const form = document.getElementById("profile-form");
+
+const btnNext = document.querySelector(".btn-next");
+
+const userName = document.getElementById("user-name");
+const errorUserName = document.getElementById("error-userName");
+
+userName.addEventListener("focus", changeStyle);
+userName.addEventListener("blur", validate);
+
+function changeStyle() {}
+
+const userData = {};
+const errorMessagge = {};
+
+function validate() {
+  console.log("blur works!");
+  if (userName.value.trim() === "" || userName.value === null) {
+    errorMessagge.userName = "Name is required";
+    console.log();
+  }
+}
 
 //  Function profile
-let requirement = function () {
-  for (const profile of form) {
-    if (profile.value.trim() === "") {
-      profile.style.border = "2px solid red";
-      profile.placeholder = "Field required";
-    }
-  }
-};
+// let requirement = function () {
+//   for (const profile of form) {
+//     if (profile.value.trim() === "") {
+//       profile.style.border = "2px solid red";
+//       profile.placeholder = "Field required";
+//     }
+//   }
+// };
 
 //Button next
-btnNext.addEventListener("click", (e) => {
-  e.preventDefault();
-  requirement();
-});
+// btnNext.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   requirement();
+// });
