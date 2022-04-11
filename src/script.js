256610e806b25d224bce1125c256b67180e02144
jose-cuevas <jgutifer@gmail.com>
2022-04-11 16:35:33 +0200

    js profile

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Select Elements
+// Profile Elements
 const form = document.getElementById("profile-form");
 
 const btnNext = document.querySelector(".btn-next");
@@ -8,21 +8,73 @@ const btnNext = document.querySelector(".btn-next");
 const userName = document.getElementById("user-name");
 const errorUserName = document.getElementById("error-userName");
 
+const userEmail = document.getElementById("email");
+const errorEmail = document.getElementById("error-email");
+
+const userPassword = document.getElementById("password");
+const errorPassword = document.getElementById("error-password");
+
+const confirmPassword = document.getElementById("confirm-password");
+const errorConfirmPassword = document.getElementById("error-confirmPassword");
+
+// Profile Events
 userName.addEventListener("focus", changeStyle);
-userName.addEventListener("blur", validate);
+userName.addEventListener("blur", validateProfile);
+
+userEmail.addEventListener("focus", changeStyle);
+userEmail.addEventListener("blur", validateProfile);
+
+userPassword.addEventListener("focus", changeStyle);
+userPassword.addEventListener("blur", validateProfile);
+
+confirmPassword.addEventListener("focus", changeStyle);
+confirmPassword.addEventListener("blur", validateProfile);
 
 function changeStyle() {}
 
 const userData = {};
-const errorMessagge = {};
+// const errorMessage = {};
 
-function validate() {
-  console.log("blur works!");
+function validateProfile() {
+  // UserName Validation
   if (userName.value.trim() === "" || userName.value === null) {
-    errorMessagge.userName = "Name is required";
-    console.log();
+    errorUserName.textContent = "Name is required";
+  } else if (userName.value.length < 5 || userName.value.length > 20) {
+    errorUserName.textContent = "Name length between 5 and 20 characters";
+  } else if (userName.value.includes(" ")) {
+    errorUserName.textContent = "Name can´t have space";
+  } else {
+    errorUserName.style.display = "none";
+    userData.username = userName.value;
+  }
+
+  // Email validation
+  if (!userEmail.value.includes("@") || userEmail.value.length > 50) {
+    errorEmail.textContent = "Invalid email";
+  } else {
+    errorEmail.style.display = "none";
+    userData.userEmail = userEmail.value;
+  }
+
+  // Password validation
+  if (
+    userPassword.value.includes(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$"
+    )
+  ) {
+    errorPassword.textContent = "Invalid password";
+    console.log(userPassword.value);
+  }
+
+  // Password confirmation
+  if (userPassword.value !== confirmPassword.value) {
+    errorConfirmPassword.value = "Error! different passwords";
+  } else {
+    userData.password = confirmPassword.value;
   }
 }
+
+console.log(userData);
 
 //  Function profile
 // let requirement = function () {
